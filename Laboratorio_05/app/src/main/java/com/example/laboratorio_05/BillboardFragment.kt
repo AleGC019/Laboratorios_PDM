77680e617716195dbac739b2465dcc4029cddbf4
AleGC019 <00083521@uca.edu.sv>
2023-04-19 15:20:36 -0600

    :memo:Laboratorio 05

---
+++ Laboratorio_05/app/src/main/java/com/example/laboratorio_05/BillboardFragment.kt
@@ -0,0 +1,48 @@
+package com.example.laboratorio_05
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.navigation.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class BillboardFragment : Fragment() {
+
+    private lateinit var actionNavtoCreateMovie: FloatingActionButton
+    private lateinit var actionNavtoShowMovieInfo: CardView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_billboard, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        bind(view)
+
+        actionNavtoCreateMovie.setOnClickListener(){
+            it.findNavController().navigate(R.id.action_billboardFragment_to_createMovieFragment)
+
+        }
+
+        actionNavtoShowMovieInfo.setOnClickListener(){
+            it.findNavController().navigate(R.id.action_billboardFragment_to_movieFragment)
+        }
+
+    }
+
+    private fun bind(view: View){
+        actionNavtoCreateMovie = view.findViewById(R.id.floatingActionButton)
+        actionNavtoShowMovieInfo = view.findViewById(R.id.cardMovie)
+
+    }
+
+
+}
---
+++ Laboratorio_05/app/src/main/java/com/example/laboratorio_05/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.laboratorio_05
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.laboratorio_05.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
