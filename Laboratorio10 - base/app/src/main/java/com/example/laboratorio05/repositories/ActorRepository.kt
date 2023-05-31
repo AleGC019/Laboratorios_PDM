e141e742f4aa12a1c962ba7095f3a9fcba1a0e0b
AleGC019 <00083521@uca.edu.sv>
2023-05-31 15:24:28 -0600

    :memo: Done Laboratorio 10

---
+++ Laboratorio10 - base/app/src/main/java/com/example/laboratorio05/repositories/ActorRepository.kt
@@ -0,0 +1,13 @@
+package com.example.laboratorio05.repositories
+
+import com.example.laboratorio05.data.dao.ActorDao
+import com.example.laboratorio05.data.model.ActorModel
+
+class ActorRepository(private val actorDao: ActorDao) {
+
+    suspend fun getActors() = actorDao.getAllActors()
+
+    suspend fun addActor(actor: ActorModel) = actorDao.insertActor(actor)
+
+
+}
